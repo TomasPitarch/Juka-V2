a7bea12c6af467dd5fa5ebb55bc5bd21e1bee650
TomyPitarch <tomypitarch@gmail.com>
2025-03-28 02:12:29 -0300

    Folder reorder character scripts

--- Assets/Scripts/Character/V.cs
+++ Assets/Scripts/Character/V.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+
 public class V : MonoBehaviourPun
 {
     [SerializeField]
