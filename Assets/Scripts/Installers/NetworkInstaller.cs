11f52bf8a614662b7fa5d1804003fbf8660b93dc
TomyPitarch <tomypitarch@gmail.com>
2025-04-05 05:12:23 -0300

    Update Installers with new dependencies

--- Assets/Scripts/Installers/NetworkInstaller.cs
+++ Assets/Scripts/Installers/NetworkInstaller.cs
@@ -1,11 +1,18 @@
+using System;
+using Photon.Pun;
+using UnityEngine;
 using Zenject;
 
 public class NetworkInstaller : MonoInstaller<NetworkInstaller>
 {
+    [SerializeField] 
+    private PhotonGameServerService photonGameServerService;
     public override void InstallBindings()
     {
         Container.Bind<INetworkService>().To<PhotonNetworkService>().AsSingle();
         Container.Bind<ILoginService>().To<PhotonLoginService>().AsSingle();
+        Container.Bind<IGameServerService>().FromInstance(photonGameServerService).AsSingle();
+        
     }
 }
 
